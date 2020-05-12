2e4661fddd11c358c6ec7a4cbad5c2e9fa85fe25
jiwonan <s2018s31@e-mirim.hs.kr>
2020-05-12 12:22:57 +0900

    별찍기★

--- startree/Program.cs
+++ startree/Program.cs
@@ -10,6 +10,18 @@ namespace startree
     {
         static void Main(string[] args)
         {
+            for(int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < ((9 - i) * 2 - 1) / 2; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int j = 0; j < (i+1) * 2 - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
